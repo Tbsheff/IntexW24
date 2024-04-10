2f0c0a795ba63de4955e71dabc4de6cac9e6fce5
burkepeterson02 <131825863+burkepeterson02@users.noreply.github.com>
2024-04-09 20:56:09 -0600

    about page

---
+++ Intex/Infrastructure/HtmlHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+
+namespace Intex.Infrastructure
+{
+	public static class HtmlHelper
+	{
+		public static string IsActive(this IHtmlHelper html, string action, string controller)
+		{
+			var routeData = html.ViewContext.RouteData;
+
+			var routeAction = (string)routeData.Values["action"];
+			var routeController = (string)routeData.Values["controller"];
+
+			// Both must match.
+			var returnActive = controller == routeController && action == routeAction;
+
+			return returnActive ? "active-menu" : "";
+		}
+	}
+}
