49dcf573357a1e495676ab2e22bbbea8ae4bc2d8
Tbsheff <92263166+Tbsheff@users.noreply.github.com>
2024-04-10 08:20:28 -0600

    fixed cart

--- Intex/Pages/Cart.cshtml.cs
+++ Intex/Pages/Cart.cshtml.cs
@@ -8,30 +8,29 @@ namespace Intex.Pages
     public class CartModel : PageModel
     {
         private ILegoRepository _repo;
-        private readonly ILogger _logger;
-        public CartModel(ILegoRepository temp, ILogger logger) 
+        public CartModel(ILegoRepository temp) 
         {
             _repo = temp;
-            _logger = logger;
         }
         public Cart? Cart { get; set; }
         public void OnGet()
         {
-            
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+   
         }
 
-        public IActionResult OnPost(int product_id)
+        public void OnPost(int product_id) 
         {
-            _logger.LogInformation($"{product_id}");
             Product pro = _repo.Products
                 .FirstOrDefault(x => x.product_id == product_id);
 
-            if (pro != null)
+            if(pro != null)
             {
-                Cart.AddItem(pro, 1); // Adjust quantity based on form input
+                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                Cart.AddItem(pro, 1);
+                HttpContext.Session.SetJson("cart", Cart);
             }
 
-            return RedirectToPage();
         }
     }
 }
