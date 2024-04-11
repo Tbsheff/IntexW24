b0a71de7258e73a5494d113d4c73d37fb8a1e1ee
burkepeterson02 <131825863+burkepeterson02@users.noreply.github.com>
2024-04-11 15:36:00 -0600

    fraud full deployment!

---
+++ Intex/Models/PredictionInput.cs
@@ -0,0 +1,40 @@
+﻿namespace Intex.Models
+{
+    public class PredictionInput
+    {
+        public int CustomerID { get; set; }
+        public int Time { get; set; }
+        public int Amount { get; set; }
+        public int Age { get; set; }
+        public int DayOfWeekMon { get; set; }
+        public int DayOfWeekSat { get; set; }
+        public int DayOfWeekSun { get; set; }
+        public int DayOfWeekThu { get; set; }
+        public int DayOfWeekTue { get; set; }
+        public int DayOfWeekWed { get; set; }
+        public int EntryModePIN { get; set; }
+        public int EntryModeTap { get; set; }
+        public int TypeOfTransactionOnline { get; set; }
+        public int TypeOfTransactionPOS { get; set; }
+        public int CountryOfTransactionIndia { get; set; }
+        public int CountryOfTransactionRussia { get; set; }
+        public int CountryOfTransactionUSA { get; set; }
+        public int CountryOfTransactionUnitedKingdom { get; set; }
+        public int ShippingAddressIndia { get; set; }
+        public int ShippingAddressRussia { get; set; }
+        public int ShippingAddressUSA { get; set; }
+        public int ShippingAddressUnitedKingdom { get; set; }
+        public int BankHSBC { get; set; }
+        public int BankHalifax { get; set; }
+        public int BankLloyds { get; set; }
+        public int BankMetro { get; set; }
+        public int BankMonzo { get; set; }
+        public int BankRBS { get; set; }
+        public int TypeOfCardVisa { get; set; }
+        public int GenderM { get; set; }
+        public int CountryOfResidenceIndia { get; set; }
+        public int CountryOfResidenceRussia { get; set; }
+        public int CountryOfResidenceUSA { get; set; }
+        public int CountryOfResidenceUnitedKingdom { get; set; }
+    }
+}
--- Intex/Pages/ReviewOrder.cshtml.cs
+++ Intex/Pages/ReviewOrder.cshtml.cs
@@ -1,17 +1,22 @@
 ﻿using Intex.Infrastructure;
 using Intex.Models;
 using Intex.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
 
 namespace Intex.Pages;
 
 public class ReviewOrder : PageModel
 {
     private ILegoRepository _repo;
+    private readonly InferenceSession _session;
     public ReviewOrder(ILegoRepository temp)
     {
         _repo = temp;
+        _session = new InferenceSession(@"decision_tree_model.onnx");
     }
     public Cart? Cart { get; set; }
     
@@ -82,6 +87,108 @@ public class ReviewOrder : PageModel
         // Clear the session and redirect to a confirmation page
         HttpContext.Session.SetJson("cart", new Cart());
         HttpContext.Session.SetJson("address", new Address());
-        return RedirectToPage("/OrderConfirmation");
+
+        Customer customer = _repo.Customers.Where(x => x.customer_ID == order.customer_ID).FirstOrDefault();
+
+        PredictionInput input = new PredictionInput
+        {
+            CustomerID = order.customer_ID,
+            Time = order.hour,
+            Amount = order.amount,
+            Age = customer.age,
+            DayOfWeekMon = (order.day_of_week == "Monday" ? 1 : 0),
+            DayOfWeekSat = (order.day_of_week == "Saturday" ? 1 : 0),
+            DayOfWeekSun = (order.day_of_week == "Sunday" ? 1 : 0),
+            DayOfWeekThu = (order.day_of_week == "Thursday" ? 1 : 0),
+            DayOfWeekTue = (order.day_of_week == "Tuesday" ? 1 : 0),
+            DayOfWeekWed = (order.day_of_week == "Wednesday" ? 1 : 0),
+            EntryModePIN = 0,
+            EntryModeTap = 0,
+            TypeOfTransactionOnline = 1,
+            TypeOfTransactionPOS = 0,
+            CountryOfTransactionIndia = 0,
+            CountryOfTransactionRussia = 0,
+            CountryOfTransactionUSA = 1,
+            CountryOfTransactionUnitedKingdom = 0,
+            ShippingAddressIndia = 0,
+            ShippingAddressRussia = 0,
+            ShippingAddressUnitedKingdom = 0,
+            ShippingAddressUSA = 1,
+            BankHSBC = (order.bank_id == 6 ? 1 : 0),
+            BankHalifax = (order.bank_id == 4 ? 1 : 0),
+            BankLloyds = (order.bank_id == 2 ? 1 : 0),
+            BankMetro = (order.bank_id == 7 ? 1 : 0),
+            BankMonzo = (order.bank_id == 5 ? 1 : 0),
+            BankRBS = (order.bank_id == 1 ? 1 : 0),
+            TypeOfCardVisa = (order.card_type_id == 1 ? 1 : 0),
+            GenderM = (customer.gender == "M" ? 1 : 0),
+            CountryOfResidenceIndia = (customer.country_of_residence == "India" ? 1 : 0),
+            CountryOfResidenceRussia = (customer.country_of_residence == "Russia" ? 1 : 0),
+            CountryOfResidenceUnitedKingdom = (customer.country_of_residence == "United Kingdom" ? 1 : 0),
+            CountryOfResidenceUSA = (customer.country_of_residence == "USA" ? 1 : 0)
+        };
+
+        int fraudulent = Predict(input.CustomerID, input.Time, input.Amount, input.Age, input.DayOfWeekMon,
+            input.DayOfWeekSat, input.DayOfWeekSun, input.DayOfWeekThu, input.DayOfWeekTue, input.DayOfWeekWed,
+            input.EntryModePIN, input.EntryModeTap, input.TypeOfTransactionOnline, input.TypeOfTransactionPOS,
+            input.CountryOfTransactionIndia, input.CountryOfTransactionRussia, input.CountryOfTransactionUSA,
+            input.CountryOfTransactionUnitedKingdom, input.ShippingAddressIndia, input.ShippingAddressRussia,
+            input.ShippingAddressUSA, input.ShippingAddressUnitedKingdom, input.BankHSBC, input.BankHalifax,
+            input.BankLloyds, input.BankMetro, input.BankMonzo, input.BankRBS, input.TypeOfCardVisa, input.GenderM,
+            input.CountryOfResidenceIndia, input.CountryOfResidenceRussia, input.CountryOfResidenceUSA,
+            input.CountryOfResidenceUnitedKingdom);
+
+        if (fraudulent == 1)
+        { 
+            return RedirectToPage("/OrderConfirmation"); 
+        }
+        else
+        {
+            return RedirectToPage("/OrderUnderReview");
+        }
+    }
+
+    public int Predict(int customer_ID, int time, int amount, int age, int day_of_week_Mon,
+       int day_of_week_Sat, int day_of_week_Sun, int day_of_week_Thu,
+       int day_of_week_Tue, int day_of_week_Wed, int entry_mode_PIN,
+       int entry_mode_Tap, int type_of_transaction_Online,
+       int type_of_transaction_POS, int country_of_transaction_India,
+       int country_of_transaction_Russia, int country_of_transaction_USA,
+       int country_of_transaction_United_Kingdom, int shipping_address_India,
+       int shipping_address_Russia, int shipping_address_USA,
+       int shipping_address_United_Kingdom, int bank_HSBC, int bank_Halifax,
+       int bank_Lloyds, int bank_Metro, int bank_Monzo, int bank_RBS,
+       int type_of_card_Visa, int gender_M, int country_of_residence_India,
+       int country_of_residence_Russia, int country_of_residence_USA,
+       int country_of_residence_United_Kingdom)
+    {
+        
+            var input = new List<float> {customer_ID, time, amount, age, day_of_week_Mon,
+            day_of_week_Sat, day_of_week_Sun, day_of_week_Thu,
+            day_of_week_Tue, day_of_week_Wed, entry_mode_PIN,
+            entry_mode_Tap, type_of_transaction_Online,
+            type_of_transaction_POS, country_of_transaction_India,
+            country_of_transaction_Russia, country_of_transaction_USA,
+            country_of_transaction_United_Kingdom, shipping_address_India,
+            shipping_address_Russia, shipping_address_USA,
+            shipping_address_United_Kingdom, bank_HSBC, bank_Halifax,
+            bank_Lloyds, bank_Metro, bank_Monzo, bank_RBS,
+            type_of_card_Visa, gender_M, country_of_residence_India,
+            country_of_residence_Russia, country_of_residence_USA,
+            country_of_residence_United_Kingdom };
+
+            var inputTensor = new DenseTensor<float>(input.ToArray(), new[] { 1, input.Count });
+
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", inputTensor)
+            };
+
+            using (var results = _session.Run(inputs))
+            {
+                var prediction = results.FirstOrDefault(item => item.Name == "output_label")?.AsTensor<long>().ToArray();
+                var fraudulent = (int)prediction[0];
+                return fraudulent;
+            }            
     }
 }
